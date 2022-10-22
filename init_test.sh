c87dd6d41bdc3c1c5888a8332c15097efd55a17a
Zhang <32409286+zkcpku@users.noreply.github.com>
2022-10-22 17:18:57 +0800

    init test

---
+++ init_test.sh
@@ -0,0 +1,22 @@
+rm -r from/
+rm -r to/
+mkdir from/
+mkdir to/
+
+touch from/1.txt
+touch from/1.log
+touch from/1.py
+
+touch from/.gitignore
+echo "*.log" > from/.gitignore
+
+mkdir from/1
+
+touch from/1/1.txt
+touch from/1/1.log
+touch from/1/1.py
+
+touch to/2.txt
+touch to/2.log
+touch to/2.py
+echo "*.txt" > to/.gitignore
